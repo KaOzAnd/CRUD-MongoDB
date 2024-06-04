93c365680c350308dcc0d7a9d6fb1078ee6cf5c8
KaOzAnd <andresjgsalzte@gmail.com>
2024-06-04 10:22:35 -0500

    Cambio duracion token

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -3,7 +3,7 @@ import { token_secret } from "../config.js";
 
 export function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, token_secret, { expiresIn: "1d" }, (err, token) => {
+    jwt.sign(payload, token_secret, { expiresIn: "2h" }, (err, token) => {
       if (err) reject(err);
       resolve(token);
     });
