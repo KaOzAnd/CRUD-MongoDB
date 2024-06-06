bdc35ff35f1c4522dc934c3ba29cd20dfbb21873
KaOzAnd <andresjgsalzte@gmail.com>
2024-06-06 17:05:11 -0500

    chore: Update App.jsx to include protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from './pages/Homepage';
 import TasksPage from './pages/TasksPage';
 import TaskFormPage from './pages/TaskFormPage';
 import ProfilePage from './pages/ProfilePage';
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
@@ -16,10 +17,13 @@ function App() {
           <Route path="/" element={<HomePage/>} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+
+          <Route element={<ProtectedRoute/>}>
           <Route path="/tasks" element={<TasksPage/>} />
           <Route path="/add-task" element={<TaskFormPage/>} />
           <Route path="/tasks/:id" element={<TasksPage/>} />
           <Route path="/profile" element={<ProfilePage/>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
