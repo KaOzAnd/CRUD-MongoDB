8d51ef0da97d52b877a6fcc9de8df7d802461c57
KaOzAnd <andresjgsalzte@gmail.com>
2024-06-06 16:56:25 -0500

    feat: Update routes in App.jsx to use dedicated page components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,23 @@ import { AuthProvider } from "./context/AuthContext";
 
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import HomePage from './pages/Homepage';
+import TasksPage from './pages/TasksPage';
+import TaskFormPage from './pages/TaskFormPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="/" element={<HomePage/>} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/tasks" element={<h1>Tasks page</h1>} />
-          <Route path="/add-task" element={<h1>New task</h1>} />
-          <Route path="/tasks/:id" element={<h1>update task</h1>} />
-          <Route path="/profile" element={<h1>Profile page</h1>} />
+          <Route path="/tasks" element={<TasksPage/>} />
+          <Route path="/add-task" element={<TaskFormPage/>} />
+          <Route path="/tasks/:id" element={<TasksPage/>} />
+          <Route path="/profile" element={<ProfilePage/>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
